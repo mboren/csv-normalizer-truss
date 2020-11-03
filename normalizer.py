d581b77637ccca445706dd014f19b3c12e225ddc
Marty Boren <martyboren@gmail.com>
2020-11-03 11:40:25 -0800

    Write output to stdout
    Except for unicode validation, the rows are currently unmodified.
    so
        $ ./normalizer.py < sample.csv > output.csv
    should create an output.csv that is identical to sample.csv

--- normalizer.py
+++ normalizer.py
@@ -7,7 +7,14 @@ if __name__ == '__main__':
     # read all lines from stdin as binary, so we don't crash on invalid utf8
     data = sys.stdin.buffer.read()
     data = data.decode('utf-8', 'replace')
-    reader = csv.DictReader(data.splitlines())
 
-    for line in reader:
-        print(line)
+    reader = csv.DictReader(data.splitlines())
+    writer = csv.DictWriter(sys.stdout,
+                            reader.fieldnames,
+                            delimiter=',',
+                            quotechar='"',
+                            quoting=csv.QUOTE_MINIMAL)
+
+    writer.writeheader()
+    for row in reader:
+        writer.writerow(row)
