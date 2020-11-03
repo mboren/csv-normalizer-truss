4116f1ddd9dc12e95df3306a3d82716d534d07a6
Marty Boren <martyboren@gmail.com>
2020-11-03 12:11:56 -0800

    Convert timestamp column

--- normalizer.py
+++ normalizer.py
@@ -2,6 +2,20 @@
 
 import sys
 import csv
+import pytz
+import datetime
+
+
+def convert_timestamp(original_timestamp: str) -> str:
+    """Parse timestamp from text, convert from US/Pacific to US/Eastern time zone,
+    and change to RFC3339 format
+    """
+    pacific = pytz.timezone('US/Pacific')
+    eastern = pytz.timezone('US/Eastern')
+    original_time = datetime.datetime.strptime(original_timestamp, '%m/%d/%y %I:%M:%S %p')
+    original_time = pacific.localize(original_time)
+    return original_time.astimezone(eastern).isoformat()
+
 
 if __name__ == '__main__':
     # read all lines from stdin as binary, so we don't crash on invalid utf8
@@ -17,4 +31,5 @@ if __name__ == '__main__':
 
     writer.writeheader()
     for row in reader:
+        row['Timestamp'] = convert_timestamp(row['Timestamp'])
         writer.writerow(row)
--- tests/test_normalizer.py
+++ tests/test_normalizer.py
@@ -1,2 +1,18 @@
 import normalizer
 
+
+class TestConvertTimestamp:
+    def test_simple_cast_dst(self):
+        input_text = '4/1/11 11:00:00 AM'
+        expected = '2011-04-01T14:00:00-04:00'
+        assert normalizer.convert_timestamp(input_text) == expected
+
+    def test_year_transition(self):
+        input_text = '12/31/16 11:59:59 PM'
+        expected = '2017-01-01T02:59:59-05:00'
+        assert normalizer.convert_timestamp(input_text) == expected
+
+    def test_dst_transition(self):
+        input_text = '3/12/16 11:01:00 PM'
+        expected = '2016-03-13T03:01:00-04:00'
+        assert normalizer.convert_timestamp(input_text) == expected
