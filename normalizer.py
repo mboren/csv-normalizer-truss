3d78f94bff55d0f7550a2d2574a6640fa035d827
Marty Boren <martyboren@gmail.com>
2020-11-03 11:34:11 -0800

    Read and parse CSV data from stdin
    I was hoping to be able to read input and write to output
    line by line, but reading everything from stdin first makes it
    easier to deal with invalid UTF-8 characters.

--- normalizer.py
+++ normalizer.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 
+import sys
+import csv
+
+if __name__ == '__main__':
+    # read all lines from stdin as binary, so we don't crash on invalid utf8
+    data = sys.stdin.buffer.read()
+    data = data.decode('utf-8', 'replace')
+    reader = csv.DictReader(data.splitlines())
+
+    for line in reader:
+        print(line)
